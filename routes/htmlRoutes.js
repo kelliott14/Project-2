bad0dcc2d7bdd90df2ba7fe4448ecb2b7b9b5f7c
Michael Osgarby <michaelosgarby@Michaels-iMac.local>
2019-11-14 08:07:16 +1100

    commented out unneeded code from passport.js authentication

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,64 +1,73 @@
 var db = require("../models");
-var jwt = require('jsonwebtoken');
-var auth = require('../auth/auth');
+var jwt = require("jsonwebtoken");
+var auth = require("../auth/auth");
 
-module.exports = function (app) {
-  // login
-  app.get("/", function (req, res) {
-    res.render("index", {
-      user: req.user
+module.exports = function(app) {
+    // login
+    app.get("/", function(req, res) {
+        res.render("index", {
+            user: req.user
+        });
     });
-  });
 
-  app.post("/api/login", function (req, res) {
-    db.User.findOne({
-      where: {
-        user_name: req.body.userLogin
-      }
-    }).then(function (results) {
-      // console.log(results);
-      if (results && req.body.userPassword == results.password) {
-        var token = jwt.sign({ user: results.id }, 'secret', { expiresIn: 10000000 });
-        res.cookie('token', token);
-        console.log(results.id);
-        res.cookie('userId', results.id);
-        // localStorage.setItem("userId", results.id)
-        res.render('dashboard');
-      } else {
-        res.render('index', {
-          message: 'authentication error'
-        })
-      }
-    })
-  });
+    app.post("/api/login", function(req, res) {
+        db.User.findOne({
+            where: {
+                user_name: req.body.userLogin
+            }
+        }).then(function(results) {
+            // console.log(results);
+            if (results && req.body.userPassword == results.password) {
+                var token = jwt.sign({ user: results.id }, "secret", {
+                    expiresIn: 10000000
+                });
+                res.cookie("token", token);
+                userLoggedIn.setId(results.id);
+                // console.log(results.id);
+                res.cookie("userId", results.id);
+                // localStorage.setItem("userId", results.id)
+                res.render("dashboard");
+            } else {
+                res.render("index", {
+                    message: "authentication error"
+                });
+            }
+        });
+    });
 
-  // dashboard
-  app.get("/dashboard", auth, function (req, res) {
-    res.render("dashboard");
-  });
+    // dashboard
+    app.get("/dashboard", auth, function(req, res) {
+        // console.log(userLoggedIn.getId());
+        res.render("dashboard");
+    });
 
-  // game list
-  app.get("/chooseGame", auth, function (req, res) {
-    res.render("chooseGame");
-  });
+    // game list
+    app.get("/chooseGame", auth, function(req, res) {
+        // console.log(userLoggedIn.getId());
+        res.render("chooseGame");
+    });
 
-  // play game
-  app.get("/playGame", auth, function (req, res) {
-    res.render("playGame");
-  });
+    // play game
+    app.get("/playGame", auth, function(req, res) {
+        // console.log(userLoggedIn.getId());
+        res.render("playGame");
+    });
 
-  // admin game list
-  app.get("/adminGameList", auth, function (req, res) {
-    res.render("adminGameList");
-  });
+    // admin game list
+    app.get("/adminGameList", auth, function(req, res) {
+        // console.log(userLoggedIn.getId());
+        res.render("adminGameList");
+    });
 
-  // admin edit
-  app.get("/adminEdit", auth, function (req, res) {
-    res.render("adminEdit");
-  });
+    // admin edit
+    app.get("/adminEdit", auth, function(req, res) {
+        // console.log(userLoggedIn.getId());
+        res.render("adminEdit");
+    });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+        // console.log(userLoggedIn.getId());
+        res.render("404");
+    });
 };

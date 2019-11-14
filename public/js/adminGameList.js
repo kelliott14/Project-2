3371e568806bcb8ac7f0d1c85d8d321b283eb7eb
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-11-14 14:37:44 +1100

    changes to admin game list page

--- public/js/adminGameList.js
+++ public/js/adminGameList.js
@@ -1,26 +1,41 @@
+var backgroundColor = ["#2a8e9d", "#ff5245", "#374050", "#ffc938"];
 $(document).ready(function() {
   $.get("/api/games", function(dbGames) {
-    dbGames.forEach(function(item) {
+    var j = 0;
+    for (i = 0; i < dbGames.length; i++) {
+      var eachCard = $("<div class='card text-white mb-3 editGameCard'></div>");
+      $(eachCard).attr("data-id", dbGames[i].id);
       var isDraft;
-      if (item.draft_status) {
+      if (dbGames[i].draft_status) {
         isDraft = "Draft";
+        $(eachCard).html(
+          "<div class='card-body'><h4 class='card-title editGameCardTitle'>" +
+            dbGames[i].title +
+            "<img class = 'draftIcon' src='/assets/drafticon.png' width='30' height='30'" +
+            "</h4><h5 class='card-title editGameCardStatus'>" +
+            isDraft +
+            "</h5></div>"
+        );
       } else {
         isDraft = "Active";
+        $(eachCard).html(
+          "<div class='card-body'><h4 class='card-title editGameCardTitle'>" +
+            dbGames[i].title +
+            "</h4><h5 class='card-title editGameCardStatus'>" +
+            isDraft +
+            "</h5></div>"
+        );
       }
-      var eachCard = $("<div class='card text-white mb-3 editGameCard'></div>");
-      $(eachCard).attr("data-id", item.id);
 
-      $(eachCard).html(
-        "<div class='card-body'><h4 class='card-title editGameCardTitle'>" +
-          item.title +
-          "</h4><h5 class='card-title editGameCardStatus'>" +
-          isDraft +
-          "</h5></div>"
-      );
-
+      if (j === 4) {
+        j = 0;
+      }
+      $(eachCard).css("background-color", backgroundColor[j]);
       $(".gameListColumns").append(eachCard);
-    });
+      j++;
+    }
   });
+  // <span aria-hidden="true">&times;</span>
 
   $("body").on("click", ".editGameCard", function() {
     var gameID = $(this).attr("data-id");

a0eaa39c1c5903a516a804c8307b8eedc724a71d
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-11-14 13:39:57 +1100

    edits to choose game page

--- public/js/chooseGame.js
+++ public/js/chooseGame.js
@@ -1,19 +1,25 @@
+var backgroundColor = ["#2a8e9d", "#ff5245", "#374050", "#ffc938"];
+
 $(document).ready(function() {
   /* eslint-disable prettier/prettier */
   //modal
 
-  $.get("/api/games", function(dbGames) {
-    dbGames.forEach(function(item){
+  $.get("/api/games", function(dbGames) {    
+    var j = 0;
+    for (var i = 0; i < dbGames.length; i++) {
       var eachCard = $("<div class='card text-white mb-3 chooseGameCard'></div>");
-      $(eachCard).attr("data-id", item.id);
-      
+      $(eachCard).attr("data-id", dbGames[i].id);
       $(eachCard).html("<div class='card-body'><h4 class='card-title chooseGameCardTitles'>"
-        + item.title + "</h4><h5 class='card-title chooseGameCardTime'>"
-        + item.ends_at + "</h5></div>");  
-    
+          + dbGames[i].title + "</h4><h5 class='card-title chooseGameCardTime'>"
+          + dbGames[i].ends_at + "</h5></div>");
+          
+      if (j === 4) {
+        j = 0;  
+      }
+      $(eachCard).css("background-color", backgroundColor[j]);
       $(".gameListColumns").append(eachCard);
-    });
-
+      j++;
+    }
   });
 
   $("body").on("click", ".card-body", function() {

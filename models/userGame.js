a6fd5134305b68cc72b5419a4fbd88e7f8e8debb
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-11-15 13:16:12 +1100

    and again

--- models/userGame.js
+++ models/userGame.js
@@ -1,28 +1,28 @@
 /* eslint-disable camelcase */
 module.exports = function(sequelize, DataTypes) {
   var UserGame = sequelize.define("UserGame", {
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "users",
-        key: "id"
-      },
-      allowNull: false
-      // validate: {
+    // user_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: "users",
+    //     key: "id"
+    //   },
+    //   allowNull: false
+    //   // validate: {
 
-      // }
-    },
-    game_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "games",
-        key: "id"
-      },
-      allowNull: false
-      // validate: {
+    //   // }
+    // },
+    // game_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: "games",
+    //     key: "id"
+    //   },
+    //   allowNull: false
+    //   // validate: {
 
-      // }
-    },
+    //   // }
+    // },
     game_points: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -48,5 +48,9 @@ module.exports = function(sequelize, DataTypes) {
       // }
     }
   });
+  UserGame.associate = function(models){
+    UserGame.belongsTo(models.User, {foreignKey: {allowNull: false}})
+    UserGame.belongsTo(models.Game, {foreignKey: {allowNull: false}})
+  };
   return UserGame;
 };

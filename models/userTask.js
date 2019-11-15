952e66884219ae32d42ab101ba0f6cd64fc64884
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-11-15 13:18:59 +1100

    getting closer

--- models/userTask.js
+++ models/userTask.js
@@ -1,27 +1,31 @@
 /* eslint-disable camelcase */
 module.exports = function(sequelize, DataTypes) {
   var UserTask = sequelize.define("UserTask", {
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "users",
-        key: "id"
-      }
-    },
-    task_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "tasks",
-        key: "id"
-      }
-    },
+    // user_id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   references: {
+    //     model: "users",
+    //     key: "id"
+    //   }
+    // },
+    // task_id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   references: {
+    //     model: "tasks",
+    //     key: "id"
+    //   }
+    // },
     task_done: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
       allowNull: false
     }
   });
+  UserGame.associate = function(models){
+    UserGame.belongsTo(models.User, {foreignKey: {allowNull: false}})
+    UserGame.belongsTo(models.Task, {foreignKey: {allowNull: false}})
+  };
   return UserTask;
 };
